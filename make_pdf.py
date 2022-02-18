f3917ec00633616676895218a2178ee0d9716d78
yuki-2000 <88224293+yuki-2000@users.noreply.github.com>
2022-02-18 14:28:18 +0900

    数字を含まないファイルに対応

--- make_pdf.py
+++ make_pdf.py
@@ -2,6 +2,20 @@ import img2pdf
 from pathlib import Path
 import re
 
+
+#数字がない文字列は前に持ってきている。
+#逆にしたいときは代2引数にfloat('inf')を指定
+#https://note.nkmk.me/python-sort-num-str/
+def extract_num(s, ret=0):
+    p = re.compile(r'(\d+)')    
+    search = p.search(s)
+    if search:
+        return int(search.groups()[-1])
+        print(search.groups())
+    else:
+        return ret
+
+
 #単処理
 def ImageToPdf(outputpath, imagepath):
     '''
@@ -13,9 +27,10 @@ def ImageToPdf(outputpath, imagepath):
     #Pathlib.WindowsPath()をstring型に変換しないとエラー
     #jpegにも対応
     imgpath_list = [str(i) for i in lists if i.match("*.jpg") or i.match("*.png") or i.match("*.jpeg")]
+    
     #https://note.nkmk.me/python-sort-num-str/
     #0埋めされていない番号順に対応
-    imgpath_list.sort(key=lambda s: int(re.findall(r'\d+', s)[-1]))
+    imgpath_list.sort(key=lambda s: extract_num(s))
     
     #pdfを作成
     with open(outputpath,"wb") as f:
